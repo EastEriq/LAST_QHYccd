719c06532ffa488fb9955c7d89f68c95f18006f7
Enrico Segre <enrico.segre@weizmann.ac.il>
2021-03-11 16:03:27 +0200

    add warning don't use in comments of takeLiveSeq

--- +inst/@QHYccd/takeLiveSeq.m
+++ +inst/@QHYccd/takeLiveSeq.m
@@ -1,5 +1,9 @@
 function imgs=takeLiveSeq(QC,num,expTime)
 % attempting once more to take live images, this time with the SDK 21-02-01
+% NOT WORKING, can hang or crash matlab in creative ways, depending on camera (600 or 367,
+%  USB port or cable), depending probably on low level USB communication issues, DDR
+%  issues, and wrong call sequence of SDK functions.
+% Don't use in real life.
 
     imgs={};
     
@@ -9,7 +13,7 @@ function imgs=takeLiveSeq(QC,num,expTime)
 
     if contains(QC.CameraName,'QHY600')
         SetQHYCCDStreamMode(QC.camhandle,1);
-        InitQHYCCD(QC.camhandle);
+        %InitQHYCCD(QC.camhandle);
     else
         InitQHYCCD(QC.camhandle);
         SetQHYCCDStreamMode(QC.camhandle,1);

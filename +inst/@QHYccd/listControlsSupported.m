9d0cf018ae065fc9efc7b3ba70da2277abed902c
Enrico Segre <enrico.segre@weizmann.ac.il>
2022-05-04 13:15:24 +0300

    listControlsSupported reporting parameter range

--- +inst/@QHYccd/listControlsSupported.m
+++ +inst/@QHYccd/listControlsSupported.m
@@ -4,11 +4,20 @@ function listControlsSupported(QC)
     [m,s]=enumeration('inst.qhyccdControl');
     fprintf('Controls supported:\n===================\n');
     for i=1:length(m)
-        if IsQHYCCDControlAvailable(QC.camhandle,m(i))
-            fprintf('XX');
+        control=m(i);
+        available=IsQHYCCDControlAvailable(QC.camhandle,m(i));
+        [ret,minV,maxV,stepV]=GetQHYCCDParamMinMaxStep(QC.camhandle,control);
+        if available
+            fprintf('AVAIL ');
         else
-            fprintf('++');
+            fprintf('UNAVA ');
         end
+        if ret==0
+            fprintf('OK ')
+        else
+            fprintf('ERR ')
+        end
+        fprintf('[%05d:%05d:%05d]',minV,stepV,maxV)
         fprintf(' %s\n',s{i});
     end
 end

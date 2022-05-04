ba45aebb755d59ad8f0126596e0a368194d10a96
Enrico Segre <enrico.segre@weizmann.ac.il>
2022-05-04 13:46:13 +0300

    add header comments to listControlsSupported

--- +inst/@QHYccd/listControlsSupported.m
+++ +inst/@QHYccd/listControlsSupported.m
@@ -1,6 +1,14 @@
 function listControlsSupported(QC)
     % list whether each possible qhyccdControl is supported or not
     %  by this camera (this is a debugging utility, not an API method)
+    % I can't make really too much out of this report though. Some
+    %  controls return unavailable, even though we are using them
+    %  all the time, (notably CONTROL_EXPOSURE) and respond with a
+    %  meaningful range; other return available,
+    %  but reading their parameter range errors and returns zeros.
+    % It might be that the report depends on the status of the camera
+    %  at the moment of the query, or simply as usual, that everything is
+    %  just fouled up
     [m,s]=enumeration('inst.qhyccdControl');
     fprintf('Controls supported:\n===================\n');
     for i=1:length(m)

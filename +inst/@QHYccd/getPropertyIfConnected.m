bd216abf0b38b51a826501a1ee2f23439a784ca6
The LAST Ocs <>
2025-06-01 09:12:43 +0000

    check camhandle empty before isNull(camhandle) in getPropertyIfConnected

--- +inst/@QHYccd/getPropertyIfConnected.m
+++ +inst/@QHYccd/getPropertyIfConnected.m
@@ -2,7 +2,7 @@ function value=getPropertyIfConnected(QC,property)
 % wrapper, which attempts to read a property value only if the serial
 % resource is defined and open, so to avoid unnecessary "cannot read"
 % error messages, e.g. when polling continuously
-if ~isNull(QC.camhandle)
+if ~isempty(QC.camhandle) && ~isNull(QC.camhandle)
     value=QC.(property);
 else
     value=[];

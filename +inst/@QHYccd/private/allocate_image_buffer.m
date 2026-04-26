e4aff2837b2f9d95f2fbe49f9179806c269f8425
Enrico Segre <enrico.segre@weizmann.ac.il>
2026-04-26 16:22:17 +0300

    .allocate_image_buffer imlength calculation not based on GetQHYCCDMemLength
    should be ok for bw images rather than the former which is wasteful, but
    doesn't consider color images with 3 bytes/pixel

--- +inst/@QHYccd/private/allocate_image_buffer.m
+++ +inst/@QHYccd/private/allocate_image_buffer.m
@@ -6,7 +6,17 @@ function allocate_image_buffer(QC)
     %  time much shorter. However, the SDK doesn't provide a safe way
     %  to determine this size, and hence we allocate a lot to stay
     %  safe from segfaults.
-    imlength=GetQHYCCDMemLength(QC.camhandle);
+    % this renders invariably imglength=9700*6522*4 (doh?) for the QHY600
+    %  which instead has a 9600*6422*2bytes sensor
+    %imlength=GetQHYCCDMemLength(QC.camhandle);
+    % alternative computation (unsafe Re: color)
+    roi=QC.ROI; % not suported by 2021 SDK
+    if isempty(roi)
+        imlength=Q.physical_size.nx * Q.physical_size.ny * QC.BitDepth/2;
+        % consider also QC.effective_area which removes overscans
+    else
+        imlength=(roi(3)-roi(1)+1) * (roi(4)-roi(2)+1)* QC.BitDepth/8;
+    end
     if QC.StreamMode==1 && QC.SharedRingBufferDim>0
         try
             QC.pImg= POSIXipc.shm(sprintf('C%s_image_ringbuffer_1',...

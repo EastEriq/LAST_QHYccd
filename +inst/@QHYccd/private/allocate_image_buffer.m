9a105979d4041b43c7060fe7597c965117a13eaf
The LAST Ocs <>
2026-04-27 15:37:55 +0000

    correction imlength in allocate_image_buffer for old SDK

--- +inst/@QHYccd/private/allocate_image_buffer.m
+++ +inst/@QHYccd/private/allocate_image_buffer.m
@@ -6,22 +6,21 @@ function allocate_image_buffer(QC)
     %  time much shorter. However, the SDK doesn't provide a safe way
     %  to determine this size, and hence we allocate a lot to stay
     %  safe from segfaults.
-    % this renders invariably imglength=9700*6522*4 (doh?) for the QHY600
+    % this renders invariably imglength=9700*6522*4 (doh!?) for the QHY600,
     %  which instead has a 9600*6422*2bytes sensor
     %imlength=GetQHYCCDMemLength(QC.camhandle);
     % alternative computation (unsafe Re: color)
-    roi=QC.ROI; % not suported by 2021 SDK
+    roi=QC.ROI; % not suported by SDK 21.7.16.13
     if isempty(roi)
-        imlength=QC.physical_size.nx * QC.physical_size.ny * QC.BitDepth/2;
+        imlength=QC.physical_size.nx * QC.physical_size.ny * QC.BitDepth/8;
         % consider also QC.effective_area which removes overscans
     else
         imlength=(roi(3)-roi(1)+1) * (roi(4)-roi(2)+1)* QC.BitDepth/8;
     end
     if QC.StreamMode==1 && QC.SharedRingBufferDim>0
         try
-            QC.pImg= POSIXipc.shm(sprintf('C%s_image_ringbuffer_1',...
-                                   QC.Id), imlength);% in case it was ealier [] or libpointer
-            for i=2:QC.SharedRingBufferDim
+            QC.pImg= POSIXipc.shm.empty; % in case it was earlier [] or libpointer
+            for i=1:QC.SharedRingBufferDim
                 QC.pImg(i)=POSIXipc.shm(sprintf('C%s_image_ringbuffer_%d',...
                                                  QC.Id, i), imlength);
             end
